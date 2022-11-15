23e22c2ec2557750cc07eeb913b7db7b332a041e
Yuri Mataev <yuri@wickfire.com>
2022-11-14 21:49:38 -0500

    fix loop logic

--- modules/postcss-extended.js
+++ modules/postcss-extended.js
@@ -1,41 +1,43 @@
 const postcss = require('postcss');
 
 function splitSelectors(nodes) {
-    for (const i in nodes) {
+    for (let i = 0; i < nodes.length; i++) {
         const node = nodes[i]
+        // if this node has nodes, recurse
+        if ('nodes' in node && node.nodes.length > 0) {
+            node.nodes = splitSelectors(node.nodes);
+        }
+
+        const replacementNodes = []
+        const nodeSerialized = JSON.stringify(node)
+
         // split selectors
         if ('selector' in node) {
             const selectors = node.selector.split(',')
             if (selectors.length > 1) {
-                const replacementNodes = []
-                const nodeSerialized = JSON.stringify(node)
                 for (const selector of selectors) {
                     const nodeDeepCopy = JSON.parse(nodeSerialized)
                     nodeDeepCopy.selector = selector.trim()
                     replacementNodes.push(nodeDeepCopy);
                 }
-                // replace this node with the replacement nodes
-                nodes.splice(i, 1, ...replacementNodes)
             }
         }
         // split media queries
         else if (node.name == 'media') {
             if ('nodes' in node && node.nodes.length > 1) {
-                const replacementNodes = []
-                const nodeSerialized = JSON.stringify(node)
                 for (const subnode of node.nodes) {
                     const nodeDeepCopy = JSON.parse(nodeSerialized)
                     const subnodeSerialized = JSON.stringify(subnode)
                     nodeDeepCopy.nodes = [ JSON.parse(subnodeSerialized) ]
                     replacementNodes.push(nodeDeepCopy);
                 }
-                // replace this node with the replacement nodes
-                nodes.splice(i, 1, ...replacementNodes)
             }
         }
-        // if this node has nodes, recurse
-        if ('nodes' in node && node.nodes.length > 0) {
-            node.nodes = splitSelectors(node.nodes);
+
+        // replace this node with the replacement nodes
+        if (replacementNodes.length > 0) {
+            nodes.splice(i, 1, ...replacementNodes)
+            i += replacementNodes.length - 1
         }
     }
     return nodes
